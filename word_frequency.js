8246e4ee03719f9e16703dfea7f52f2ee72f3b82
Megan Cain <145371841+megancain@users.noreply.github.com>
2024-01-09 16:55:09 -0500

    uploaded

--- word_frequency.js
+++ word_frequency.js
@@ -1,48 +1,75 @@
-const fs = require('fs')
-const path = require('path')
-const filePath = process.argv[2]
+const fs = require("fs");
+const path = require("path");
 
-const STOP_WORDS = [
-  'a',
-  'an',
-  'and',
-  'are',
-  'as',
-  'at',
-  'be',
-  'by',
-  'for',
-  'from',
-  'has',
-  'he',
-  'i',
-  'in',
-  'is',
-  'it',
-  'its',
-  'of',
-  'on',
-  'that',
-  'the',
-  'to',
-  'were',
-  'will',
-  'with',
-]
+// Get file path from command line arguments
+const filePath = process.argv[2];
 
-function printWordFreq(file, callback) {
-  // Read in `file` and print out the frequency of words in that file.
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading the file:', err)
-      process.exit(1)
-    }
-    // TODO: write code to count the words in the file
-    console.log('Initial data read from file: ', data)
-    callback('Your word count results should be passed in to this callback')
-  })
+// List of stop words
+const STOP_WORDS = new Set([
+  "a",
+  "an",
+  "and",
+  "are",
+  "as",
+  "at",
+  "be",
+  "by",
+  "for",
+  "from",
+  "has",
+  "he",
+  "i",
+  "in",
+  "is",
+  "it",
+  "its",
+  "of",
+  "on",
+  "that",
+  "the",
+  "to",
+  "were",
+  "will",
+  "with",
+]);
+
+// Read file content asynchronously
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
 }
 
-printWordFreq(filePath, (wordCount) => {
-  console.log('The results from your word counts:', wordCount)
-})
+// Count word frequency, excluding stop words
+function countWordFrequency(text) {
+  const wordCount = {};
+  text
+    .toLowerCase()
+    .split(/\W+/)
+    .forEach((word) => {
+      if (!STOP_WORDS.has(word) && word) {
+        wordCount[word] = (wordCount[word] || 0) + 1;
+      }
+    });
+  return wordCount;
+}
+
+// Main function to process the file and print word frequency
+async function processFile(filePath) {
+  try {
+    const data = await readFileAsync(filePath);
+    const wordCount = countWordFrequency(data);
+    console.log("Word Frequencies:", wordCount);
+  } catch (error) {
+    console.error("Error reading the file:", error.message);
+  }
+}
+
+// Execute the main function with the provided file path
+processFile(filePath);
